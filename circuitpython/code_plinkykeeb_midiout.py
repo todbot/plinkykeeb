20f4336cd81f2ccd1e4b024bee29e3b6e953b02b
Tod Kurt <todbot@users.noreply.github.com>
2022-02-10 22:13:48 -0800

    some cirpy updates

--- circuitpython/code_plinkykeeb_midiout.py
+++ circuitpython/code_plinkykeeb_midiout.py
@@ -9,13 +9,18 @@ import time
 import board
 import keypad
 import busio
+import neopixel
+import rainbowio
 import usb_midi
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
+from adafruit_midi.pitch_bend import PitchBend
 
 # config
-note_base = 36
+octave = 3
 note_velocity = 127
+
+leds = neopixel.NeoPixel(board.MISO, 20, brightness=0.2, auto_write=False)
 
 midi_uart = busio.UART(tx=board.TX, rx=None, baudrate=31250 ) # timeout=0.01)
 midi_serial = adafruit_midi.MIDI(midi_out=midi_uart)
@@ -27,18 +32,70 @@ col_pins = (board.D2, board.D3, board.D4, board.D5, board.D6)
 
 km = keypad.KeyMatrix( row_pins=row_pins, column_pins=col_pins )
 
+# I like millis
+def millis(): return time.monotonic()*1000
+
+# scale a value by amount/256, expects an int, returns an int
+def scale8(val,amount):
+    return (val*amount)//256
+# dim all leds by an (amount/256)
+def dim_leds_by(leds, amount):
+    leds[0:] = [[max(scale8(i,amount),0) for i in l] for l in leds]
+
+spin_hue = False
+oct_switch = False
+
+led_last_update_millis = 0
+led_millis = 5
+
 while True:
+    now = millis()
+    if now - led_last_update_millis > led_millis:
+        led_last_update_millis = now
+        dim_leds_by(leds, 250)
+        leds.show()
+
+    if spin_hue:
+        hue = (time.monotonic() * 100 ) % 256
+        print(hue)
+        leds.fill( rainbowio.colorwheel( int(hue) ) )
+
+
     event = km.events.get()
     if event:
         print("key:%d %d/%d %d" % (event.key_number, event.pressed, event.released, event.timestamp) )
         #print("\t\t", dir(event))
-        note_val = event.key_number + note_base
-        if event.pressed:
-            noteon = NoteOn(note_val, note_velocity)
-            midi_serial.send( noteon )
-            midi_usb.send( noteon )
-        if event.released:
-            noteoff = NoteOn(note_val, 0)
-            midi_serial.send( noteoff )
-            midi_usb.send( noteoff )
         
+        if event.key_number < 17:
+            note_base = octave * 12
+            note_val = event.key_number + note_base
+            if event.pressed:
+                noteon = NoteOn(note_val, note_velocity)
+                print("note on:",note_val)
+                midi_serial.send( noteon )
+                midi_usb.send( noteon )
+                leds[ event.key_number ] = rainbowio.colorwheel( time.monotonic() * 20 )
+            if event.released:
+                noteoff = NoteOn(note_val, 0)
+                midi_serial.send( noteoff )
+                midi_usb.send( noteoff )
+        else:
+            spin_hue = event.pressed
+            if event.key_number == 19: # octave up/dn enable
+                oct_switch = event.pressed
+
+            # this is dumb, pitch up should be +8191, pitch middle 0, pitch down -8192
+            if event.key_number == 18: # pitchbend up
+                if oct_switch:
+                    octave = octave+1
+                else:
+                    pb_val = PitchBend(16383) if event.pressed else PitchBend(8192)
+                    midi_serial.send( pb_val )
+                    midi_usb.send( pb_val )
+            if event.key_number == 17: # pitchbend down
+                if oct_switch:
+                    octave = octave-1
+                else:
+                    pb_val = PitchBend(0) if event.pressed else PitchBend(8192)
+                    midi_serial.send( pb_val )
+                    midi_usb.send( pb_val )
--- circuitpython/code_plinkykeeb_test2.py
+++ circuitpython/code_plinkykeeb_test2.py
@@ -16,7 +16,6 @@ row_pins = (board.D7, board.D8, board.D9, board.D10)
 col_pins = (board.D2, board.D3, board.D4, board.D5, board.D6)
 
 km = keypad.KeyMatrix( row_pins=row_pins, column_pins=col_pins )
-print("here")
 
 # scale a value by amount/256, expects an int, returns an int
 def scale8(val,amount):
@@ -25,16 +24,29 @@ def scale8(val,amount):
 def dim_leds_by(leds, amount):
     leds[0:] = [[max(scale8(i,amount),0) for i in l] for l in leds]
 
-leds.fill(0x00ff00)
+leds.fill( rainbowio.colorwheel(60) )
 leds.show()
 time.sleep(1)
+
+spin_hue = False
 
 while True:
     dim_leds_by(leds, 250)
     leds.show()
+
+    if spin_hue:
+        hue = (time.monotonic() * 100 ) % 256
+        print(hue)
+        leds.fill( rainbowio.colorwheel( hue ) )
+        leds.show()
+
     event = km.events.get()
+
     if event:
-        print("key:%d %d/%d %d" % (event.key_number, event.pressed, event.released, event.timestamp) )
+        print("key:%d %d/%d %d %d" % (event.key_number, event.pressed, event.released, event.timestamp) )
         leds[ event.key_number ] = rainbowio.colorwheel( time.monotonic() * 20 )
-        #print("\t\t", dir(event))
+
+        if event.key_number == 19:
+            spin_hue = event.pressed
+                
 
