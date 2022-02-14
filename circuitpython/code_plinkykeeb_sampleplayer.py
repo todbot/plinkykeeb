90e852360c3b8588419c3c310b0c6b0e124c9a81
Tod Kurt <todbot@users.noreply.github.com>
2022-02-14 13:31:45 -0800

    some new sampleplayer thoughts

--- circuitpython/code_plinkykeeb_sampleplayer.py
+++ circuitpython/code_plinkykeeb_sampleplayer.py
@@ -13,6 +13,8 @@ print("plinkykeeb_sampleplayer!")
 import time
 import board
 import keypad
+import neopixel
+import rainbowio
 
 import audiocore
 import audiomixer
@@ -38,10 +40,12 @@ wav_files = (
     (None, None),                       # B
     ('wav/amenfull_22k_s16.wav', True), # C
     ('wav/amen1_22k_s16.wav', True),    # C#
-    ('wav/ohohoh1.wav', True),          # D
+    ('wav/ohohoh2.wav', True),          # D
     ('wav/amen2_22k_s16.wav', True),    # D#
     (None, None),                       # E
 )
+
+leds = neopixel.NeoPixel(board.MISO, 20, brightness=0.2, auto_write=False)
 
 # pins used by keyboard
 row_pins = (board.D7, board.D8, board.D9, board.D10)
@@ -56,6 +60,24 @@ mixer = audiomixer.Mixer(voice_count=len(wav_files), sample_rate=22050, channel_
 # attach mixer to audio playback
 audio.play(mixer)
 
+# # load all the wavs into RAM
+# wavs = [None] * len(wav_files)
+# for i in range(len(wavs)):
+#     (wav_file, loopit) = wav_files[i]
+#     if wav_file is not None:
+#         wavs[i] = audiocore.WaveFile(open(wav_file,"rb"))
+
+# # play or stop a RAM WAV sample using the mixer
+# def handle_sample_wavs(num, pressed):
+#     voice = mixer.voice[num]   # get mixer voice
+#     (wav_file, loopit) = wav_files[num]
+#     if pressed:
+#         if wav_file is not None:
+#             voice.play(wavs[num],loop=loopit)
+#     else: # released
+#         if loopit:
+#             voice.stop()  # only stop looping samples, others one-shot
+
 # play or stop a sample using the mixer
 def handle_sample(num, pressed):
     voice = mixer.voice[num]   # get mixer voice
@@ -67,16 +89,46 @@ def handle_sample(num, pressed):
     else: # released
         if loopit:
             voice.stop()  # only stop looping samples, others one-shot
- 
+
+
+# I like millis
+def millis(): return time.monotonic()*1000
+
+# scale a value by amount/256, expects an int, returns an int
+def scale8(val,amount):
+    return (val*amount)//256
+# dim all leds by an (amount/256)
+def dim_leds_by(leds, amount):
+    leds[0:] = [[max(scale8(i,amount),0) for i in l] for l in leds]
+
+leds.fill(0x333333)
+
+led_last_update_millis = 0
+led_millis = 5
+
+keys_pressed = []  # list of keys currently being pressed down
 while True:
+    now = millis()
+    
+    # update the LEDs, but only if keys pressed
+    if now - led_last_update_millis > led_millis:
+        led_last_update_millis = now
+        for i in keys_pressed:  # light up those keys that are pressed
+            leds[ i ] = rainbowio.colorwheel( int(time.monotonic() * 20) )
+        dim_leds_by(leds, 250)  # fade everyone out slowly
+        leds.show()
+    
     event = km.events.get()
+    
     if event:
         print("key:%d %d/%d %d" % (event.key_number, event.pressed, event.released, event.timestamp) )
 
         if event.key_number < len(wav_files):
             if event.pressed:
                 handle_sample( event.key_number, True )
+                keys_pressed.append( event.key_number )
 
             if event.released:
                 handle_sample( event.key_number, False )
+                keys_pressed.remove( event.key_number )
         
